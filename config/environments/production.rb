b91229320654c8f3c52f7c1d2d543c9fd9b9d2ab
Gary Riger <grigerny@aol.com>
2012-08-05 21:01:18 -0400

    got rid of users email for devise install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,5 @@ Railsapp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = { :host => 'http://sleepy-plateau-1805.herokuapp.com' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120805231254) do
+ActiveRecord::Schema.define(:version => 20120806005953) do
 
   create_table "comments", :force => true do |t|
     t.string   "name"
@@ -91,7 +91,6 @@ ActiveRecord::Schema.define(:version => 20120805231254) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
