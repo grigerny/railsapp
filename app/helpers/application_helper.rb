f6f087c425e55ef293a5ce2f5199213fb630b449
Gary Riger <grigerny@aol.com>
2012-08-05 21:45:35 -0400

    added gravatar and fixed comments in provider#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  module UrlHelper
+    def url_with_protocol(url)
+      /^http/.match(url) ? url : "http://#{url}"
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def gravatar_for email, options = {}
+      options = {:alt => 'avatar', :class => 'avatar', :size => 80}.merge! options
+      id = Digest::MD5::hexdigest email.strip.downcase
+      url = 'http://www.gravatar.com/avatar/' + id + '.jpg?s=' + options[:size].to_s
+      options.delete :size
+      image_tag url, options
+  end
 end
